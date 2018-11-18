613d117ead8d1588e78828f68e6354aebdd89757
root <root@hpc-certification.org>
2018-11-18 00:09:27 +0000

    Update

--- dev/update.sh
+++ dev/update.sh
@@ -5,12 +5,17 @@ cd /home/www/hpccertification/skill-tree-wiki
 git pull -q
 
 git add wiki/data/media wiki/data/pages
-
 git commit -m "autocommit"
+git push -q
 
 
 cd skill-tree
+
+git add -A *
+git commit -m "autocommit from the webpage"
 git pull -q
+git push -q
+
 
 ) > /dev/null
 
