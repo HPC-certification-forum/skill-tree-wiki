d184fb5efc64afe436a528fe7e3593445ecf52e1
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-06-05 15:58:41 +0100

    Create links automatically using JavaScript

---
+++ wiki/lib/plugins/hpccf/script.js
@@ -0,0 +1,22 @@
+function hpccf_create_link(link, text){
+  return '<li class="level1"><div class="li"> <a href="' + link + '" class="wikilink1">' + text + '</a></div></li>'
+}
+
+function hpccf_mod_links(links){
+  var div = jQuery("div.page");
+  if(! div){
+    return ;
+  }
+  div.append('<h1 class="sectionedit10" id="Links">Links</h1>');
+  var path = window.location.pathname;
+  path = path.split("/");
+  path.shift();
+  path.shift();
+  path = path.join("/");
+
+  links = hpccf_create_link("/contribute-question/" + path, "Submit a proposal for an examination question");
+
+  div.append('<div class="level1"><ul>' + links + "</ul></div>");
+}
+
+jQuery( document ).ready(hpccf_mod_links);
