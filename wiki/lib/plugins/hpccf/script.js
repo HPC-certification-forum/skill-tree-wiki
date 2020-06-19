75b3d3db9a7073d9f9a3715714f7ae28c918ee60
root <root@hpc-certification.org>
2020-06-19 20:50:50 +0000

    Support manual adding of skills.

--- wiki/lib/plugins/hpccf/script.js
+++ wiki/lib/plugins/hpccf/script.js
@@ -8,6 +8,10 @@ function hpccf_is_star(path){
   return c.search(path + ";") > -1;
 }
 
+function hpccf_add_star_option(path){
+  return '<span id="marked" class="' + (hpccf_is_star(path) ? "star" : "") + '" onclick="hpccf_star(\'' + path + '\');">&#x2605;</span>'
+}
+
 function hpccf_star(path){
   var o = jQuery("span#marked");
   o.toggleClass("star");
@@ -24,26 +28,122 @@ function hpccf_star(path){
   jQuery.cookie("likeSkill", c, { path: '/' });
 }
 
-function hpccf_show_selection(){
-  var div = jQuery("#selected_skills");
-  if(div.length == 0){
-    return ;
+hpccf_skill = [];
+hpccf_skill_title = {};
+
+function hpccf_remove_skill(id){
+  var pos = hpccf_skill.indexOf(id);
+  if (pos >= 0) hpccf_skill.splice(pos, 1);
+}
+
+function hpccf_renderSkillData(data){
+  var title = data["title"];
+  var id = data["id"];
+  if(id){
+    var elem = jQuery("#skill_" + id.replace(/[/]/g, "_"));
+    if(elem.length == 0){
+      return;
+    }
+    if(title){
+      elem.empty().append(title);
+      hpccf_skill_title[id] = title;
+    }else{
+      elem.empty().append("<span style='color:red;'>invalid</span>");
+      hpccf_remove_skill(id);
+    }
+  }else{
+    console.log("error retrieving data");
+    console.log(data);
   }
-  var c = jQuery.cookie("likeSkill");
-  if(! c || c.length == 0){
-    div.parent().append("<p>None selected</p>");
+}
+
+function hpccf_renderSkill(url){
+  jQuery.ajax({
+    url: "https://www.hpc-certification.org/api/" + url + "?fields=title,id",
+    dataType: "json",
+    mimeType: "application/json",
+    success: hpccf_renderSkillData,
+    error: function(jqXHR, textStatus, error) {
+      console.log("jqXHR: '", jqXHR, "'");
+      console.log("textStatus: '", textStatus, "'");
+      console.log("error: '", error, "'");
+    }
+  });
+}
+
+function hpccf_create_endorsment(){
+  var out = jQuery("#output");
+  out.show();
+  out.empty().append("<h2>Output</h2>");
+  var first = true;
+  var str = "";
+  for(var i in hpccf_skill){
+    var skill = hpccf_skill[i];
+    if(! skill in hpccf_skill_title) continue;
+    if(! first) str += ",<br>";
+    str += skill + " (" + hpccf_skill_title[skill] + ")";
+    first = false;
+  }
+  out.append("<p>This training covers (partially)<br>" + str + " <div>https://hpc-certification.org/wiki/skill-tree/</div></p>");
+}
+
+function hpccf_load_skill_list(){
+  var skills = jQuery("#userskills").val().split(/[\n;,]/);
+  var pending = [];
+  for(var i in skills){
+    var s = skills[i].trim();
+    if(s.length == 0) continue;
+    // check if the skill was previously selected
+    if(hpccf_skill.indexOf(s) > -1) continue;
+    hpccf_skill.push(s);
+  }
+
+  hpccf_skill.sort();
+  hpccf_show_selection();
+}
+
+function hpccf_load_skills_cookie(){
+  var arr = jQuery.cookie("likeSkill").split(";");
+  arr.pop();
+  hpccf_skill = arr.sort();
+}
+
+function hpccf_show_selection(){
+  var div = jQuery("#skill_selection");
+  if(div.length == 0){
+    div = jQuery("#selected_skills");
+    if(div.length == 0){
+      return ;
+    }
+    div.parent().append("<div id='skill_selection'></div");
+    div = jQuery("#skill_selection");
+  }else{
+    div.empty();
+  }
+  var input = "<h2>Manual add skills to the selection</h2><p>List/Paste here valid skill IDs and leave the box</p><p><textarea id='userskills' onchange='hpccf_load_skill_list();' rows='5' cols='40'></textarea></p>";
+  if(hpccf_skill.length == 0){
+    div.append("<p>None selected</p>" + input);
     return;
   }
-  var str = "";
-  var selected = c.split(";");
-  for(x in selected){
-    var skill = selected[x];
+  var str = "<div><b>ID</b></div>";
+  var str2 = "<div><b>Title</b></div>";
+  var str3 = "<div><b>Marked</b></div>";
+  for(x in hpccf_skill){
+    var skill = hpccf_skill[x];
     if(! skill) continue;
-    str + = "<li>" + skill + "</li>";
+    str + = "<div>" + skill + "</div>";
+    if( skill in hpccf_skill_title ){
+      str2 + = "<div>" + hpccf_skill_title[skill] + "</div>";
+    }else{
+      str2 + = "<div id='skill_" + skill.replace(/[/]/g, "_") + "'>&nbsp;</div>";
+      hpccf_renderSkill(skill);
+    }
+    str3 + = '<div><span class="star" onclick="hpccf_remove_skill(\'' + skill + '\'); hpccf_show_selection();">&#x2605;</span></div>';
   }
-  div.parent().append("<ul>" + str + "</ul>");
-  div.parent().append('<h2>Operations</h2>');
-  div.parent().append('<a href="#" onclick=\'jQuery.cookie("likeSkill", "", { path: "/" });location.reload();\'>Clear selection</a>');
+  div.append("<div class='skilllist'>" + str + "</div><div class='skilllist'>" + str2 + "</div><div>" + str3 + '</div><div><a href="#" onclick=\'jQuery.cookie("likeSkill", "", { path: "/" });location.reload();\'>Clear selection</a></div><p></p>' + input);
+  div.append('<h2>Operations</h2>');
+  div.append('<p><div><a href="#" onclick=\'hpccf_create_endorsment()\'>Create training endorsment string</a></div> </p>');
+  div.append('<div id="output" style="display:none"></div>');
 }
 
 function hpccf_mod_links(links){
@@ -52,6 +152,7 @@ function hpccf_mod_links(links){
     return ;
   }
 
+  hpccf_load_skills_cookie();
   hpccf_show_selection();
 
   var path = window.location.pathname;
@@ -74,7 +175,7 @@ function hpccf_mod_links(links){
   // append control on top
   div = jQuery("div.page"); //div = jQuery("h1#background");
   if(div.length != 0){
-    div.prepend('<span id="marked" class="' + (hpccf_is_star(path) ? "star" : "") + '" onclick="hpccf_star(\'' + path + '\');">&#x2605;</span>')
+    div.prepend(hpccf_add_star_option(path));
   }
 }
 
